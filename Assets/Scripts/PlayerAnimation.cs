b86ac2d4dd5bfbb9dbb576de3fcd98fa96a51ae2
GeunTae KIM <k031900@gmail.com>
2021-07-27 17:02:07 +0900

    k
    정리

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -7,84 +7,10 @@ public class PlayerAnimation : MonoBehaviour
     Animator animator;
     public GameManager gameManager;
     PlayerMovement playerMovement;
-    public bool isParryingAttack = false;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         playerMovement = transform.parent.GetComponent<PlayerMovement>();
     }
-
-    public void AttackStop()
-    {
-        playerMovement.nowCombo = 0;
-        playerMovement.canMove = true;
-
-        animator.SetBool("isAttack0", false);
-        animator.SetBool("isAttack1", false);
-        animator.SetBool("isAttack2", false);
-        playerMovement.canCombo = false;
-    }
-
-    public void DashStop()
-    {
-        animator.SetBool("isDash", false);
-        playerMovement.nowSlide = false;
-    }
-
-    public void SlideAttackStop()
-    {
-        animator.SetBool("isSlideAttack", false);
-        animator.SetBool("isDash", false);
-        playerMovement.nowSlide = false;
-    }
-
-    public void ComboStart()
-    {
-        playerMovement.canCombo = true;
-    }
-
-    public void ComboEnd()
-    {
-        playerMovement.canCombo = false;
-    }
-
-    public void HitStop()
-    {
-        animator.SetBool("isHit", false);
-        playerMovement.enough = false;
-    }
-
-    public void paringStart()
-    {
-        playerMovement.canParing = true;
-    }
-
-    public void paringEnd()
-    {
-        playerMovement.canParing = false;
-    }
-
-    public void parryAttackEnd()
-    {
-        animator.SetBool("isParryAttack", false);
-        animator.SetBool("isAttack0", false);
-        animator.SetBool("isAttack1", false);
-        animator.SetBool("isAttack2", false);
-
-        playerMovement.canMove = true;
-    }
-
-    public void DamageUp()
-    {
-        gameManager.isParredAttack = true;
-        gameManager.changeDamage();
-    }
-
-    public void DamageNormal()
-    {
-        gameManager.isParredAttack = false;
-        gameManager.changeDamage();
-    }
-
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,10 @@ public class PlayerMovement : MonoBehaviour
     public GameManager gamemanager;
 
     [Header("플레이어 속도")]
-    public float noramlSpeed;
-    public float GuardSpeed;
+    public float jumpSpeed;
     public float speed;
 
     [Space]
-    public float dashSpeed;
     public float gravity;
 
     private CharacterController controller; // 현재 캐릭터가 가지고있는 캐릭터 컨트롤러 콜라이더.
@@ -25,179 +23,16 @@ public class PlayerMovement : MonoBehaviour
     public bool canMove = true;
     public SpriteRenderer PlayerSprite;
 
-    [Header("슬라이딩 콤보어택용")]
-    public Slider slider;
-    public float minPos;
-    public float maxPos;
-    public RectTransform pass;
-    //float sliderSpeed = 100;
-    public bool comboStart = false;
-    public int nowCombo = 0;
-
-    public bool nowSlide = false;
-    public bool canCombo = false;
-
-    [Header("피격 변수")]
-    public bool enough = false;
-
-    [Header("패링 변수")]
-    public bool canParing = false;
-
-    [Header("공격과 패리 콜라이더 위치")]
-    public GameObject attackCollider;
-    public GameObject parryCollider;
-
     void Start()
     {
         MoveDir = Vector3.zero;
         controller = GetComponent<CharacterController>();
     }
 
-    void Update()
-    {
-        // 데미지 상태가 아니라면 키 입력 가능
-        if (animator.GetBool("isHit") == false)
-        {
-            // Q를 눌렀을때
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                // 대쉬 상태라면
-                if (animator.GetBool("isDash") == true)
-                {
-                    animator.SetBool("isSlideAttack", true);
-                    // 예외처리
-                    animator.SetBool("isAttack0", false);
-                    animator.SetBool("isAttack1", false);
-                    animator.SetBool("isAttack2", false);
-                }
-                // 처음 공격 상태라면
-                else if (animator.GetBool("isAttack0") == false
-                        && animator.GetBool("isAttack1") == false
-                        && animator.GetBool("isAttack2") == false)
-                {
-                    animator.SetBool("isAttack0", true);
-                    nowCombo = 0;
-                    canMove = false;
-                }
-                // 콤보 공격중이라면
-                else if (animator.GetBool("isAttack0") == true
-                        || animator.GetBool("isAttack1") == true
-                        || animator.GetBool("isAttack2") == true)
-                {
-                    // 콤보영역에 들어옴
-                    if (canCombo == true)
-                    {
-                        canCombo = false;
-                        nowCombo++;
-                        if (nowCombo == 1)
-                        {
-                            animator.SetBool("isAttack1", true);
-                        }
-                        else if (nowCombo == 2)
-                        {
-                            animator.SetBool("isAttack2", true);
-                        }
-                    }
-                    else
-                    {
-
-                    }
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                animator.SetBool("isGuard", true);
-            }
-            if (Input.GetKeyUp(KeyCode.E))
-            {
-                animator.SetBool("isGuard", false);
-            }
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-
-            }
-
-            if (animator.GetBool("isGuard") == true)
-            {
-                speed = GuardSpeed;
-            }
-            else if (canMove == true)
-            {
-                speed = noramlSpeed;
-            }
-            else
-            {
-                speed = 0;
-            }
-        }
-
-
-
-        //******************************* 슬라이드식 콤보 어택 **********************************************************
-
-        /*
-        void SetAtk()
-        {
-            slider.value = 0;
-            minPos = pass.anchoredPosition.x;
-            maxPos = pass.sizeDelta.x + minPos;
-
-            if (nowCombo <= 1)
-            {
-                nowCombo++;
-                StartCoroutine(ComboAtk());
-            }
-        }
-
-        IEnumerator ComboAtk()
-        {
-            yield return null;
-            while (!(Input.GetKeyDown(KeyCode.Q) || slider.value == slider.maxValue))
-            {
-                slider.value += Time.deltaTime * sliderSpeed;
-                yield return null;
-            }
-
-            if (slider.value >= comboMin && slider.value <= comboMax)
-            {
-                
-                if (nowCombo == 1)
-                {
-                    Debug.Log("판정 성공1");
-                    animator.SetBool("isAttack1", true);
-                }
-                else if (nowCombo == 2)
-                {
-                    Debug.Log("판정 성공2");
-                    animator.SetBool("isAttack2", true);
-                    // 돌진 넣기
-                }
-                SetAtk();
-            }
-            else
-            {
-                Debug.Log("판정실패");
-                slider.value = 0;
-                nowCombo = 0;
-            }
-
-
-        }
-        */
-
-
-    }
-
-
     private void FixedUpdate()
     {
         // 현재 캐릭터가 땅에 있고 공격상태가 아니라면
-        if (controller.isGrounded && animator.GetBool("isHit") == false)
+        if (controller.isGrounded)
         {
             if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) //하나라도 움직인다.
             {
@@ -212,33 +47,10 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetAxisRaw("Horizontal") >= 0)
                 {
                     PlayerSprite.flipX = false;
-
-                    attackCollider.transform.position = new Vector3(
-                        transform.position.x + 0.85f,
-                        transform.position.y,
-                        transform.position.z
-                        );
-
-                    parryCollider.transform.position = new Vector3(
-                        transform.position.x + 0.85f,
-                        transform.position.y,
-                        transform.position.z
-                        );
                 }                 
                 else
                 {
                     PlayerSprite.flipX = true;
-
-                    attackCollider.transform.position = new Vector3(
-                    transform.position.x - 0.85f,
-                    transform.position.y,
-                    transform.position.z);
-
-                    parryCollider.transform.position = new Vector3(
-                        transform.position.x - 0.85f,
-                        transform.position.y,
-                        transform.position.z
-                        );
                 }                 
 
                 animator.SetFloat("_Y", MoveDir.z);
@@ -261,75 +73,15 @@ public class PlayerMovement : MonoBehaviour
             // 스피드 증가.
             MoveDir *= speed;
 
-            // 캐릭터 대쉬
-            if (Input.GetButton("Jump") && nowSlide == false)
-            {
-                //슬라이드 중복 방지
-                nowSlide = true;
-
-                // 대쉬 이동
-                MoveDir.x = dashSpeed * last_x;
-                MoveDir.z = dashSpeed * last_z;
-                animator.SetBool("isDash", true);
-
-                // 공격하고 있다면 초기화
-                comboStart = false;
-                canMove = true;
-                nowCombo = 0;
-                animator.SetInteger("Combo", 0);
-
-                // 슬라이드 시작
-                //StartCoroutine(ComboAtk());
-            }
-
             // 캐릭터 점프
-            //if (Input.GetButton("Jump"))
-            //    MoveDir.y = jumpSpeed;
+            if (Input.GetButton("Jump"))
+                MoveDir.y = jumpSpeed;
         }
 
         // 캐릭터에 중력 적용.
         MoveDir.y -= gravity * Time.deltaTime;
 
         // 캐릭터 움직임.
-        if (animator.GetBool("isHit") == false)
-        {
-            controller.Move(MoveDir * Time.deltaTime);
-        }
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-            // 이미 맞은 상태가 아니라면
-            if (enough == false)
-            {
-
-            if (other.tag == "EnemyAttack")
-            {
-                if (animator.GetBool("isGuard"))
-                {
-                    if (canParing == true)
-                    {
-                        // 매니저에 정보전달
-                        gamemanager.Damage(other.GetComponent<BrainBridge>().brain.damage, "Parry");
-
-                        // 적의 모션 변경
-                        other.GetComponent<BrainBridge>().brain.Parryed();
-                    }
-                    else
-                    {
-                        gamemanager.Damage(other.GetComponent<BrainBridge>().brain.damage, "Defense");
-                    }
-                }
-                else
-                {
-                    Debug.Log("플레이어 공격받음");
-                    gamemanager.Damage(other.GetComponent<BrainBridge>().brain.damage, "Hit");
-                    animator.SetBool("isHit", true);
-                    animator.SetBool("isDash", false);
-                    nowSlide = false;
-                }
-            }
-        }
+        controller.Move(MoveDir * Time.deltaTime);
     }
 }
