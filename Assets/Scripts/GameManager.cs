f6b879bce680d8960960f730ec255c24ae115269
GeunTae KIM <k031900@gmail.com>
2021-08-15 23:41:54 +0900

    addiso

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,10 +9,12 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene("iso");
+        }
     }
 
 }
